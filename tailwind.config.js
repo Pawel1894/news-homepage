c26b1294fefda62ba1ff572bdc4e3e078112ba66
Pawel1894 <pawepohl@gmail.com>
2023-01-09 22:04:36 +0100

    Project init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: {
+        100: "hsl(35, 77%, 62%)",
+        200: "hsl(5, 85%, 63%)",
+      },
+      neutral: {
+        100: "hsl(36, 100%, 99%)",
+        200: "hsl(233, 8%, 79%)",
+        300: "hsl(236, 13%, 42%)",
+        400: "hsl(240, 100%, 5%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
